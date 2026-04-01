76ae611ba1554428c8a2f6699f4ce7d079023049
vibhuti121 <ramavibhuti121@gmail.com>
2026-04-01 09:04:38 +0530

    start of application

---
+++ src/main/java/com/ebay/flightsbooking/FlightsBookingApplication.java
@@ -0,0 +1,13 @@
+package com.ebay.flightsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightsBookingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightsBookingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebay/flightsbooking/FlightsBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebay.flightsbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightsBookingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
